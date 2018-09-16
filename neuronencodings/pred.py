1a2e82dd446ece3246ceeaa5111ea31882d55b37
nicholasturner1 <nturner.stanford@gmail.com>
2018-09-16 18:44:29 -0400

    Starting to modify pred.py, but incomplete

--- neuronencodings/pred.py
+++ neuronencodings/pred.py
@@ -1,60 +1,36 @@
-import numpy as np
-from torch.autograd import Variable
-import torch
 import os
+
+import numpy as np
 from scipy import spatial
 
-
-import models
-from datasets_pychg import CellDataset
+import torch
 
 from meshparty import mesh_io
 
-home = os.path.expanduser("~")
+import utils
+import data
+from data import CellDataset
+from data import transform
+
+
+HOME = os.path.expanduser("~")
 expt_dir = "%s/seungmount/research/nick_and_sven/models_sven/" % home
-meshmeta = mesh_io.MeshMeta()
+MESHMETA = mesh_io.MeshMeta()
 
-
-def load_model(expt_name, model_name, chkpt_num, expt_dir=expt_dir,
-               bottle_fs=128, n_points=2500, in_dim=3, bn=True, eval_=True):
-    model_class = getattr(models, model_name)
-    model = model_class(n_points, pt_dim=in_dim, bn=bn, bottle_fs=bottle_fs)
-
-    if eval_:
-        model.cuda().eval()
-    else:
-        model.cuda()
-
-    model_dir = os.path.join(expt_dir, expt_name, "models")
-
-    model_chkpt = "{dir}/model_{iter}.chkpt".format(dir=model_dir,
-                                                    iter=chkpt_num)
-    model.load_state_dict(torch.load(model_chkpt))
-
-    return model
 
 def predict_points(model, points):
-    points_batch = torch.from_numpy(points)
-    points_batch = Variable(points_batch)
-    points_batch = points_batch.cuda()
-    # points_batch = points_batch.transpose(2, 1)
+    points_tensor = torch.from_numpy(points).cuda()
 
-    points_batch = points_batch.cuda()
-    _, fs = model.forward(points_batch)
-    fs = fs.cpu().data.numpy()
-
-    return fs
+    with torch.no_grad():
+        _, fs = model.forward(points_tensor)
+    return fs.data.cpu().numpy()
 
 
-def load_orphans(n_points=2500, batch_size=2, dataset_name="pinky"):
-    if dataset_name == "pinky":
-        dataset_paths = [home + "/seungmount/research/svenmd/pointnet_orphan_axons_gt_180308_refined/"]
-    elif dataset_name == "full_cells_pinky":
-        dataset_paths = [home + "/seungmount/research/svenmd/pointnet_full_semantic_labels_masked_180401_refined/"]
-    elif dataset_name == "fish":
-        dataset_paths = [home + "/seungmount/research/svenmd/180831_meshes_ashwin_refined/"]
-    else:
-        raise()
+def load_orphans(n_points=2500, batch_size=2, 
+                 dset_name="orphan_axons_refined"):
+
+    gt_dirs = data.fetch_dset_dirs(dset_name)
+
 
     dataset = CellDataset(gt_dirs=dataset_paths,
                           phase=3,
@@ -130,9 +106,7 @@ def load_vertex_block_pychg(fnames, center_coords=None, n_points=500,
         if len(vertices) < n_points:
             vertices = vertices[np.random.choice(len(vertices), n_points, replace=True)]
 
-        # Normalize to unit sphere and mean zero
-        vertices -= np.min(vertices, axis=0)[None]
-        vertices /= np.max(np.linalg.norm(vertices, axis=1))
+        vertices = transform.norm_to_unit_sphere(vertices)
 
         vertices_list.append(vertices)
         vertex_ids_list.append(center_vertex_id)
